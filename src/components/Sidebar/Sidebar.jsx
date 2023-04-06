e17a7f3cc8772220b49dab181429ac6903c1adc0
orcatechaniket <113422927+orcatechaniket@users.noreply.github.com>
2023-04-06 09:17:53 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,62 +6,75 @@ import {
   MdProductionQuantityLimits,
   MdAddToHomeScreen,
 } from "react-icons/md";
-import {SiInstacart} from "react-icons/si"
+import { SiInstacart } from "react-icons/si";
 import { GoFileSubmodule } from "react-icons/go";
 import { FaPeopleCarry } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = ({ toggle }) => {
-    const location  = useLocation()
+  const location = useLocation();
   return (
     <div className={`${toggle ? "d-none" : "col sidebar-column"}`}>
       <div className="brand">
         <h4>Flikzo</h4>
-        <hr/>
+        <hr />
       </div>
       <ul>
-        <li  className={location.pathname === "/" ? "active" : ""}>
         <Link className="link" to="/">
-          <AiFillDashboard />
-          DashBoard
-          </Link>
-        </li>
-        <li  className={location.pathname === "/category" ? "active" : ""}>
+          <li className={location.pathname === "/" ? "active" : ""}>
+            <AiFillDashboard />
+            DashBoard
+          </li>
+        </Link>
         <Link className="link" to="/category">
-          <MdCategory />
-          Category</Link>
-        </li>
-        <li  className={location.pathname === "/sub-category" ? "active" : ""}>
+          <li className={location.pathname === "/category" ? "active" : ""}>
+            <MdCategory />
+            Category
+          </li>
+        </Link>
         <Link className="link" to="/sub-category">
-          <GoFileSubmodule />
-          Subcategory</Link>
+          <li className={location.pathname === "/sub-category" ? "active" : ""}>
+            <GoFileSubmodule />
+            Subcategory
+          </li>
+        </Link>
+          <Link className="link" to="/vendor">
+        <li className={location.pathname === "/vendor" ? "active" : ""}>
+            <FaPeopleCarry />
+            Vendor
         </li>
-        <li  className={location.pathname === "/vendor" ? "active" : ""}>
-        <Link className="link" to="/vendor">
-          <FaPeopleCarry />
-          Vendor
           </Link>
+          <Link className="link" to="/products">
+        <li
+          className={
+            location.pathname === "/products" ||
+            location.pathname === "/products/:id"
+              ? "active"
+              : ""
+          }
+        >
+            <MdProductionQuantityLimits />
+            Products
         </li>
-        <li  className={location.pathname === "/products" || location.pathname === "/products/:id" ? "active" : ""}>
-        <Link className="link" to="/products">
-          <MdProductionQuantityLimits />
-          Products</Link>
+          </Link>
+          <Link className="link" to="/home-page">
+        <li className={location.pathname === "/home-page" ? "active" : ""}>
+            <MdAddToHomeScreen />
+            Home PAge
         </li>
-        <li  className={location.pathname === "/home-page" ? "active" : ""}>
-        <Link className="link" to="/home-page">
-          <MdAddToHomeScreen />
-          Home PAge</Link>
+          </Link>
+          <Link className="link" to="/orders">
+        <li className={location.pathname === "/orders" ? "active" : ""}>
+            <SiInstacart />
+            Orders
         </li>
-        <li  className={location.pathname === "/orders" ? "active" : ""}>
-        <Link className="link" to="/orders">
-          <SiInstacart />
-          Orders</Link>
+          </Link>
+          <Link className="link" to="/user">
+        <li className={location.pathname === "/user" ? "active" : ""}>
+            <SiInstacart />
+            Users
         </li>
-        <li  className={location.pathname === "/user" ? "active" : ""}>
-        <Link className="link" to="/user">
-          <SiInstacart />
-          Users</Link>
-        </li>
+          </Link>
       </ul>
     </div>
   );
